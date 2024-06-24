5db118e7370a862f6a149afd8d0727ca64d25878
avbds002 <avbds2077@gmail.com>
2024-06-24 12:27:28 -0300

    pageable findAll()

--- backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
@@ -1,6 +1,11 @@
 package com.devsuperior.dscatalog.controllers;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +18,12 @@ import java.util.List;
 @RequestMapping(value = "/categories")
 public class CategoryController {
 
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping()
-    public ResponseEntity<List<Category>> findAll() {
-        List<Category> categoryList = new ArrayList<>();
-        categoryList.add(new Category(1L, "Books"));
-        categoryList.add(new Category(2L, "Eletronics"));
-        return ResponseEntity.ok().body(categoryList);
+    public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable) {
+        Page<CategoryDTO> categoryDTOPage = categoryService.findAll(pageable);
+        return ResponseEntity.ok(categoryDTOPage);
     }
 }
